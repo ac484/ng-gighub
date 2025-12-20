a5f77fa0da040edff330b52b2b421362ac8cfc39
7Spade <7s.i@pm.me>
2025-12-21 04:10:01 +0800

    1

--- functions-ai-document/src/index.ts
+++ functions-ai-document/src/index.ts
@@ -26,7 +26,7 @@ import { setGlobalOptions } from 'firebase-functions/v2/options';
 
 // Set global options for all functions
 setGlobalOptions({
-  region: 'asia-east1',
+  region: 'us-central1',
   maxInstances: 10
 });
 
