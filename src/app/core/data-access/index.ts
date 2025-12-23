7274bda492fa66cffdbe97e5241b6636746029cf
7s.i <7s.i@pm.me>
2025-12-24 02:09:36 +0800

    1

--- src/app/core/data-access/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Core Data Access Layer
- * Centralized data access and repository management (ADR-0002)
- */
-
-// Storage repository is still used by some modules
-export * from './repositories/storage/log-storage.repository';
